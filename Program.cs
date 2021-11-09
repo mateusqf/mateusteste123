812e940345f13c7b9a2be89c6728cdc654f559a0
mateusqf <mateusqueiroz011@gmail.com>
2021-11-09 14:28:15 -0300

    fiz outro

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace mateusgit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite um numero");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o segundo numero");
+            int numero2= int.Parse(Console.ReadLine());
+
+            if (numero1 >= 10 )
+            {
+                Console.WriteLine("vc passou");
+            }
+            else if (numero2 < 10)
+            {
+                Console.WriteLine("vc esta reprovado");
+            }
+            
+            
+            }
+    }
+}
